cae5b6fda778ae3799214012bfbd731a8af5beae
Elchairoy Meir <elchairoy@gmail.com>
2021-12-01 11:18:59 +0200

    test_useful_functions.h

---
+++ include/test_useful_functions.h
@@ -0,0 +1,6 @@
+#include "useful_functions.h"
+#include <stdio.h>
+
+/* This function tests the functions get_piece_in_square and change_the_square.
+   If there is and error, prints an error message and returns 1, otherwise returns 0. */
+int test_useful_functions();
