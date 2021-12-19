a7a99cbf39c59a30527319f9902d4cfd6961af29
chaim <chaimke2005@gmail.com>
2021-12-19 22:38:31 +0200

    sos

--- include/test_make_moves.h
+++ include/test_make_moves.h
@@ -1,7 +1,7 @@
 #ifndef B9D1A4ED_411B_42A0_A8A5_5915CCD572AB
 #define B9D1A4ED_411B_42A0_A8A5_5915CCD572AB
 
-void check_make_moves();
+int check_make_moves();
 #include "board_struct.h"
 #include "make_move.h"
 #include "useful_functions.h"
